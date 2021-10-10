72d288533ba5b9ba6a4e3ea720642a7a43eca37b
coderlui <lui.serna07@hotmail.com>
2021-10-09 23:11:23 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Product from "./components/Product";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const product = [
+        {name: "Shell Decor Necklace", price: 39900},
+        {name: "Mixed Charm Necklace", price: 27900},
+        {name: "3pcs Necklace", price: 23900},
+        {name: "Pearl Necklace", price: 29900},
+    ];
+
+    return (
+        <div className="App">
+            <NavBar></NavBar>
+            <h1 style={{color: "#7D6B7D"}}>Welcome to our Website</h1>
+            <hr />
+            {product.map(({ name, price }) => (
+                <Product key={name} name={name} price={price} />
+            ))}
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,51 @@
+import "./NavBar.css";
+
+
+const NavBar = () => {
+    return (
+
+        <><nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid menuNavegacion">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+
+            <ul className="navbar-nav justify-content-center">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="index.html">INICIO</a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="Productos.html" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">PRODUCTOS</a>
+                
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item" href="Productos.html">FAVORITOS</a></li>
+                  <li><a className="dropdown-item" href="aretes.html">ARETES</a></li>
+                  <li><a className="dropdown-item" href="collares.html">COLLARES</a></li>
+                  <li><a className="dropdown-item" href="brazaletes.html">BRAZALETES</a></li>
+                  <li><a className="dropdown-item" href="bolsos.html">BAGS</a></li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="Nosotros.html">NOSOTROS</a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="Contacto.html">CONTACTO</a>
+              </li>
+              
+              <li className="nav-item">
+                <a className="nav-link" href="Envios y devoluciones.html">ENVIOS Y DEVOLUCIONES</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav></>
+
+    );
+};
+
+export default NavBar;
